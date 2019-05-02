273a7fab8cc95bac3e105a4bf3230f7ef221655b
Ankit <ajpatel90@gmail.com>
2019-05-02 10:45:41 -0400

    successfully added routes in app

--- leagueOL/src/app/app.module.ts
+++ leagueOL/src/app/app.module.ts
@@ -6,16 +6,36 @@ import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { ChampionsComponent } from './champions/champions.component';
 import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
 
+const appRoutes: Routes = [
+  {
+    path: 'champs',
+    component: ChampionsComponent
+  },
+  {
+    path: 'about',
+    component: AboutComponent
+  },
+  {
+    path: '',
+    component:HomeComponent
+  },
+]
 @NgModule({
   declarations: [
     AppComponent,
+    HomeComponent,
     NavbarComponent,
     ChampionsComponent,
     AboutComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(
+      appRoutes,
+      {enableTracing: true}
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
